402c415c536dcb52054525cafca457e3642798ab
d4chury <15achury02@gmail.com>
2025-10-16 02:56:27 -0500

    feat: change file yml

--- src/test/java/com/company/wompi/config/EnvConfig.java
+++ src/test/java/com/company/wompi/config/EnvConfig.java
@@ -12,10 +12,19 @@ public class EnvConfig {
     // Private constructor to prevent instantiation
     private EnvConfig() {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        printLoadedConfig();
         this.sandboxUrl = dotenv.get("WOMPI_SANBOX_URL");
         this.prodUrl = dotenv.get("WOMPI_PROD_URL");
         this.publicKey = dotenv.get("WOMPI_PUBLIC_KEY");
         this.privateKey = dotenv.get("WOMPI_PRIVATE_KEY");
+    }
+
+    public void printLoadedConfig() {
+        System.out.println("🧠 [EnvConfig] Verificación de variables cargadas:");
+        System.out.println("   WOMPI_SANBOX_URL: " + (sandboxUrl != null ? "✅ Cargada" : "❌ No cargada"));
+        System.out.println("   WOMPI_PROD_URL: " + (prodUrl != null ? "✅ Cargada" : "❌ No cargada"));
+        System.out.println("   WOMPI_PUBLIC_KEY: " + (publicKey != null ? "✅ Cargada" : "❌ No cargada"));
+        System.out.println("   WOMPI_PRIVATE_KEY: " + (privateKey != null ? "✅ Cargada" : "❌ No cargada"));
     }
 
     // Public method to get the singleton instance
